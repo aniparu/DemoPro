1ba301489d559ba606013087ff635e88912403f6
aniparu <aniparu122@gmail.com>
2022-10-28 15:23:17 +0530

    some message added

---
+++ src/org/sample/Employee.java
@@ -0,0 +1,17 @@
+package org.sample;
+
+public class Employee {
+	
+	private void empId() {
+		System.out.println("employee id 234567");
+
+	}
+	
+	public static void main(String[] args) {
+		Employee e = new Employee();
+		e.empId();
+		
+		
+	}
+
+}
